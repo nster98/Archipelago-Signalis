edcb8ce767aa2934985cb5264edc41a544aef353
Nathan Glikman <nster98@gmail.com>
2025-03-02 03:32:20 -0500

    Initial testing for Http Requests

--- ArchipelagoSignalis/ArchipelagoStart.cs
+++ ArchipelagoSignalis/ArchipelagoStart.cs
@@ -4,10 +4,41 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Net.Http.Headers;
+using System.Net.Http;
+using Newtonsoft.Json.Linq;
 
 namespace ArchipelagoSignalis
 {
-    public class ArchipelagoStart : MelonMod
-    {
+    public class ArchipelagoStart : MelonMod {
+
+        public override void OnInitializeMelon()
+        {
+            base.OnInitializeMelon();
+            MelonLogger.Msg("ArchipelagoSignalis loaded");
+            FetchData().GetAwaiter().GetResult();
+        }
+
+        private async Task FetchData()
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                try
+                {
+                    HttpResponseMessage response = await client.GetAsync("https://api.github.com/repos/dotnet/runtime");
+                    response.EnsureSuccessStatusCode();
+                    string responseBody = await response.Content.ReadAsStringAsync();
+
+                    JObject json = JObject.Parse(responseBody);
+                    MelonLogger.Msg("Data fetched from GitHub API:");
+                    MelonLogger.Msg(json.ToString());
+                }
+                catch (HttpRequestException e)
+                {
+                    MelonLogger.Error($"Request error: {e.Message}");
+                }
+            }
+            
+        }
     }
 }
