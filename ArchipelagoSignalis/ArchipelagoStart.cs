e40ee44297bb5e3823d41f17a7ee5714999e3cb2
Nathan Glikman <nster98@gmail.com>
2025-03-05 02:15:53 -0500

    A whole lot of stuff (useless commit message i know)

--- ArchipelagoSignalis/ArchipelagoStart.cs
+++ ArchipelagoSignalis/ArchipelagoStart.cs
@@ -6,6 +6,8 @@ using UnityEngine;
 using Newtonsoft.Json;
 using System.Net.Http;
 using System.Threading.Tasks;
+using UnityEngine.SceneManagement;
+using Newtonsoft.Json.Linq;
 
 namespace ArchipelagoSignalis
 {
@@ -23,6 +25,7 @@ namespace ArchipelagoSignalis
             OpenStorageBoxFromAnywhereListener();
             GiveRadio();
             CheckForF9KeyPress();
+            OpenIntruderLevelSelect();
         }
 
         private static void OpenStorageBoxFromAnywhereListener()
@@ -43,10 +46,28 @@ namespace ArchipelagoSignalis
             }
         }
 
+        private List<string> intruderLevelNames = ["END", "PEN", "EXC", "MED", "ROT", "MEM", "LAB", "DET", "BIO", "RES", "LOV"];
+
         public override void OnSceneWasLoaded(int buildIndex, string sceneName)
         {
             base.OnSceneWasLoaded(buildIndex, sceneName);
-            MelonLogger.Msg($"Scene loaded: {sceneName}");
+            MelonLogger.Msg($"Scene loaded: {sceneName} build index {buildIndex}");
+
+            if (sceneName == "scenes_")
+            {
+                Scene intruderScene = SceneManager.GetActiveScene();
+                GameObject[] objects = UnityEngine.Object.FindObjectsOfType<GameObject>();
+                foreach (GameObject gameObject in objects)
+                {
+                    if (gameObject.activeInHierarchy && intruderLevelNames.Contains(gameObject.name))
+                    {
+                        if (gameObject.name == "LOV")
+                        {
+                            gameObject.SetActive(false);
+                        }
+                    }
+                }
+            }
         }
 
         // Add item to inventory or box, depending on current inventory count
@@ -90,6 +111,17 @@ namespace ArchipelagoSignalis
             }
         }
 
+        private static void OpenIntruderLevelSelect()
+        {
+            if (Input.GetKeyDown(KeyCode.F8))
+            {
+                MelonLogger.Msg("F8 key pressed");
+
+                SceneHelper sceneHelper = new SceneHelper();
+                sceneHelper.LoadSceneDirect("scenes_");
+            }
+        }
+
         private static async Task FetchDataFromApi()
         {
             using (HttpClient client = new HttpClient())
@@ -114,6 +146,7 @@ namespace ArchipelagoSignalis
             }
         }
     }
+
     public class ApiResponse
     {
         [JsonProperty("item")] public string Item { get; set; }
@@ -129,6 +162,7 @@ namespace ArchipelagoSignalis
     public static class DetectItemPickup
     {
         private static int numItemsInInventory = 0;
+
         private static void Prefix(ItemPickup __instance)
         {
             numItemsInInventory = InventoryManager.elsterItems.Count;
@@ -136,6 +170,7 @@ namespace ArchipelagoSignalis
             var playerState = PlayerState.currentRoom;
             MelonLogger.Msg($"Item picked up: {item._name} in room: {playerState.roomName}");
         }
+
         private static void Postfix(ItemPickup __instance)
         {
             var currentNumItemsInInventory = InventoryManager.elsterItems.Count;
@@ -145,12 +180,13 @@ namespace ArchipelagoSignalis
                 {
                     return;
                 }
+
                 MelonLogger.Msg($"Removed item {__instance._item.name} from inventory");
                 var item = __instance._item;
                 InventoryManager.RemoveItem(item);
                 //TODO: Call Archipelago API for check
             }
-            
+
         }
     }
 
@@ -170,5 +206,73 @@ namespace ArchipelagoSignalis
         {
             MelonLogger.Msg("Ending triggered");
         }
+    }
+
+    [HarmonyPatch(typeof(Steganography), "Encode")]
+    public static class DetectSteganography
+    {
+        private static void Prefix(Texture2D image, ref string message)
+        {
+            MelonLogger.Msg("Steganography triggered");
+            MelonLogger.Msg($"Message: {message}");
+
+            // Parse the JSON message
+            var jsonObject = JsonConvert.DeserializeObject<Dictionary<string, object>>(message);
+
+            // Add the new string to the strings array
+            var strings = jsonObject["strings"] as JArray;
+            strings.Add("Item1,Item2,Item3");
+
+            // Add the new stringKey to the stringKeys array
+            var stringKeys = jsonObject["stringKeys"] as JArray;
+            stringKeys.Add("archipelagoData");
+
+            // Serialize the modified JSON back to a string
+            message = JsonConvert.SerializeObject(jsonObject);
+            MelonLogger.Msg($"Modified Message: {message}");
+        }
+    }
+
+    [HarmonyPatch(typeof(SaveManager), "Load")]
+    public static class LoadSave
+    {
+        private static void Postfix()
+        {
+            MelonLogger.Msg("Loading save");
+            MelonLogger.Msg(SProgress.GetString("archipelagoData", ""));
+        }
+    }
+
+    [HarmonyPatch(typeof(ROT_MeatBlocker), "Block")]
+    public static class TurnOffMeatBlockers
+    {
+        public static void Postfix(ROT_MeatBlocker __instance)
+        {
+            MelonLogger.Msg("Attempting to turn off meat blockers");
+            MelonLogger.Msg(__instance.blocked);
+            __instance.blocked = false;
+            __instance.enabled = false;
+            SProgress.SetBool("Meat_" + __instance.ID, __instance.blocked);
+
+
+            foreach (var blocker in __instance.Blockers)
+            {
+                blocker.SetActive(false);
+            }
+            foreach (var unblocker in __instance.UnBlockers)
+            {
+                unblocker.SetActive(true);
+            }
+
+            foreach (var connectedDoor in __instance.Locks)
+            {
+                connectedDoor.locked = false;
+                connectedDoor.BacktrackDoor = true;
+                // could be this need to test
+                connectedDoor.externalUnlocker = true;
+                connectedDoor.UpdateProperties();
+            }
+        }
+        
     }
 }
